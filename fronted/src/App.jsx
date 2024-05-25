fc33000335508d5a113cbd0fab82da53982bcabf
unknown <jamilafzal255@gmail.com>
2024-05-25 17:21:55 +0500

    when  user is  typing other user show on is typing

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,23 +1,45 @@
-import { useEffect, useMemo, useState } from 'react'
-import { io } from 'socket.io-client'
+import { useEffect, useMemo, useState } from 'react';
+import { io } from 'socket.io-client';
+
 function App() {
-  const [inputVal, setInputVal] = useState()
-  const socket = useMemo(() => io('http://localhost:3000'), [])
+  const [inputVal, setInputVal] = useState('');
+  const [typingMessage, setTypingMessage] = useState('');
+  const socket = useMemo(() => io('http://localhost:3000'), []);
+
   useEffect(() => {
-    socket.on('connect', () => {})
-    socket.on('welcome', (payload) => console.log(payload))
-    socket.on('message', (payload) => console.log(payload))
-  }, [])
+    socket.on('connect', () => {});
+    socket.on('welcome', (payload) => console.log(payload));
+    socket.on('message', (payload) => console.log(payload));
+    socket.on('typing', (message) => setTypingMessage(message));
+    socket.on('stopTyping', () => setTypingMessage(''));
+  }, [socket]);
+
+  const handleInputChange = (e) => {
+    setInputVal(e.target.value);
+    if (e.target.value) {
+      socket.emit('typing', 'User'); // Replace 'User' with actual username if available
+    } else {
+      socket.emit('stopTyping');
+    }
+  };
 
   const submitHandler = () => {
-    socket.emit('username', inputVal)
-  }
+    socket.emit('username', inputVal);
+    setInputVal('');
+    socket.emit('stopTyping');
+  };
+
   return (
     <>
-      <input type="text" onChange={(e) => setInputVal(e.target.value)} />
-      <button onClick={submitHandler}>send</button>
+      <input
+        type="text"
+        value={inputVal}
+        onChange={handleInputChange}
+      />
+      <button onClick={submitHandler}>Send</button>
+      <div>{typingMessage}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
