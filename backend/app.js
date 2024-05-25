23f4d471f76562d61ecae106454e181a08b7d89d
unknown <jamilafzal255@gmail.com>
2024-05-25 17:22:26 +0500

    istyping

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,32 @@
-import express from 'express'
-import { createServer } from 'http'
-import { Server } from 'socket.io'
-const PORT = process.env.PORT_URL || 3000
-const app = express()
-const httpServer = createServer(app)
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const PORT = process.env.PORT_URL || 3000;
+const app = express();
+const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: '*',
   },
-})
+});
+
 io.on('connection', (socket) => {
-  socket.emit('welcome', 'Welcome to web socket')
+  socket.emit('welcome', 'Welcome to web socket');
 
   socket.on('username', (payload) => {
-    socket.broadcast.emit('message', payload)
-  })
-})
+    socket.broadcast.emit('message', payload);
+  });
+
+  socket.on('typing', (payload) => {
+    socket.broadcast.emit('typing', `${payload} is typing...`);
+  });
+
+  socket.on('stopTyping', () => {
+    socket.broadcast.emit('stopTyping');
+  });
+});
 
 httpServer.listen(PORT, () => {
-  console.log(`listening on ${PORT}`)
-})
+  console.log(`listening on ${PORT}`);
+});
